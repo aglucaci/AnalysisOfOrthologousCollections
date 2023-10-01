d2617dadea52fcda58994f48f7d970d1bbd68d60
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2023-10-01 11:01:35 -0400

    Update run_AOC_Local.sh

--- run_AOC_Local.sh
+++ run_AOC_Local.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 # Downloading hyphy-analyses.
 FOLDER="hyphy-analyses"
-
 URL="https://github.com/veg/hyphy-analyses.git"
 
 if [ ! -d "$FOLDER" ] ; then
@@ -21,28 +20,29 @@ fi
 printf "# Running AOC Snakemake pipeline \n"
 
 # Initial phase, quality control, alignment, recombination-detection.
+# Run Selection Analyses on recombination-free files
 snakemake \
       -s Snakefile \
       --cluster-config cluster.json \
-      --jobs 1 all \
+      --jobs 2 all \
       --rerun-incomplete \
       --cores all \
       --keep-going \
       --reason \
       --latency-wait 120
       
-#exit 0
+exit 0
 
 # Run Selection Analyses on recombination-free files
-snakemake \
-      -s Snakefile_Recombinants \
-      --cluster-config cluster.json \
-      --jobs 1 all \
-      --rerun-incomplete \
-      --cores all \
-      --keep-going \
-      --reason \
-      --latency-wait 120
+#snakemake \
+#      -s Snakefile_Recombinants \
+#      --cluster-config cluster.json \
+#      --jobs 1 all \
+#      --rerun-incomplete \
+#      --cores all \
+#      --keep-going \
+#      --reason \
+#      --latency-wait 120
 
 exit 0
 # End of file
